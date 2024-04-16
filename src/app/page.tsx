7fafdcc24b752bd154fd38c407705cde4b9900ba
thuyhangfr01 <thuyhangfr01@gmail.com>
2024-04-16 13:26:31 +0700

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import tasksApiRequest from "@/apiRequests/tasks";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   const res = tasksApiRequest.get();
@@ -8,6 +9,7 @@ export default async function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen">
       <Button>Click dô đây</Button>
+      <Link href="/home">Home page</Link>
       <div>
         <h1>List tasks: </h1>
         {data.map((item) => (
